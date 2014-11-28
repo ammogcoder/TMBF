83d7d5a2573603a6280fbefa590b6581d9864196
TuanDang <dangtuan21@gmail.com>
2014-11-28 10:58:13 -0600

    aaa

--- TMBF/TMBF/DAL/TelecomContext.cs
+++ TMBF/TMBF/DAL/TelecomContext.cs
@@ -10,12 +10,15 @@ namespace TMBF.DAL
             : base("TelecomContext") 
         {
         }
-
+        //aaa
         public DbSet<Customer> Customers { get; set; }
-
+        public DbSet<Someone> Someones { get; set; }
         public DbSet<Service> Services { get; set; }
+        public DbSet<User> Users { get; set; }
 
         public DbSet<SalesRep> SalesReps { get; set; }
+        public DbSet<Call> Calls { get; set; }
+
         
         protected void onModelCreating(DbModelBuilder modelBuilder)
         {
