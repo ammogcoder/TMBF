edd07dffec24af041ea2a1c48caf2a14d782617f
devilshbasha <mohamed.abdlrheem@gmail.com>
2014-11-28 11:15:07 -0500

    login

--- TMBF/TMBF/Models/SalesRep.cs
+++ TMBF/TMBF/Models/SalesRep.cs
@@ -4,9 +4,11 @@ using System.Web.Services;
 using System.Web.Services.Protocols;
 using System.ComponentModel;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TMBF.Models
 {
+    [Table("SalesRep")]
     public class SalesRep : User
     {
         public virtual ICollection<Customer> Customers { get; set; }
