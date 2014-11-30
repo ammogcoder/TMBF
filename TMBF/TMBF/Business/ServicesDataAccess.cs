b5060362bf2bd3d8bbb02d08ea76a3d8aa523e9c
pezcamaron <felipe.rojod@gmail.com>
2014-11-30 00:23:06 -0600

    rate upload working slowly

--- TMBF/TMBF/Business/ServicesDataAccess.cs
+++ TMBF/TMBF/Business/ServicesDataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,34 @@ namespace TMBF.Business
 {
     public class ServicesDataAccess
     {
-        public static Boolean insertService(string name, float peekRate, float offPeekRate, DateTime startDate, DateTime endDate, int sourceCountry, int destCountry) {
-            return true;
+        public static Boolean insertService(string name, float PeekRate, float OffPeekRate, DateTime RateEffectiveDate, DateTime RateEndDate, int SourceCountry_ID, int DestinationCountry_ID)
+        {
+            int ret = 0;
+            string sql = "INSERT INTO Service (Name,PeekRate,OffPeekRate,RateEffectiveDate ,RateEndDate ,DestinationCountry_ID ,SourceCountry_ID) VALUES (@Name,@PeekRate,@OffPeekRate,@RateEffectiveDate ,@RateEndDate ,@DestinationCountry_ID ,@SourceCountry_ID )";
+            SqlConnection connection = ConnectionManager.GetTMConnection();
+            SqlCommand command = new SqlCommand(sql, connection);
+            //command.Parameters.AddWithValue("@ID", 0);
+            command.Parameters.AddWithValue("@Name", name);
+            command.Parameters.AddWithValue("@PeekRate", PeekRate);
+            command.Parameters.AddWithValue("@OffPeekRate", OffPeekRate);
+            command.Parameters.AddWithValue("@RateEffectiveDate", RateEffectiveDate);
+            command.Parameters.AddWithValue("@RateEndDate", RateEndDate);
+            command.Parameters.AddWithValue("@DestinationCountry_ID", DestinationCountry_ID);
+            command.Parameters.AddWithValue("@SourceCountry_ID", SourceCountry_ID);
+            try
+            {
+                ret = command.ExecuteNonQuery();
+                if (connection != null) connection.Close();
+                if (ret > 0)
+                    return true;
+                else
+                    return false;
+            }
+            catch (SqlException e)
+            {
+                if (connection != null) connection.Close();
+                return false;
+            }
         }
     }
 }
