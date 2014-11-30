e9c2f171f6968c69a5f8ce964e9e4e544c89b8be
TuanDang <dangtuan21@gmail.com>
2014-11-29 18:07:34 -0600

    Report : CustomerBill

---
+++ TMBF/TMBF/Models/SearchParameterModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TMBF.Models
+{
+    public class SearchParameterModel
+    {
+        [Display(Name = "Month")]
+        public string Month { get; set; }
+        [Display(Name = "Year")]
+        public string Year { get; set; }
+    }
+}
