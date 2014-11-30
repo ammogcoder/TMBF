9df6cc216cc147ecbfb209b9be26c583f22fb77e
TuanDang <dangtuan21@gmail.com>
2014-11-30 09:15:53 -0600

    CustomerBill

--- TMBF/TMBF/Controllers/ReportController.cs
+++ TMBF/TMBF/Controllers/ReportController.cs
@@ -21,12 +21,12 @@ namespace TMBF.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public ViewResult CustomerBillReportViewer(SearchParameterModel um)
+        public ViewResult CustomerBillReportViewer(SearchParameterModel searchParameterModel)
         {
-            return View(um);
+            return View(searchParameterModel);
         }
 
-        public FileContentResult GenerateAndDisplayReport(string month, string year)
+        public FileContentResult GenerateAndDisplayReport(string month, string year, string format)
         {
             if (month == null || year == null)
                 return null;
@@ -34,17 +34,16 @@ namespace TMBF.Controllers
             localReport.ReportPath = Server.MapPath("~/Report/rpCustomerBill.rdlc");
             
             ReportDataSource reportDataSource = new ReportDataSource();
-            //reportDataSource.Name = "dsCustomerBill";
-            reportDataSource.Name = "dsColDTO";
+            reportDataSource.Name = "dsCustomerBill";
+            //reportDataSource.Name = "dsColDTO";
             //ttt
             Customer customer = new Customer();
             customer.ID = 6421234599;
             customer.PhoneNo = "23234234";
 
-            //IList<ColDTO> customerList = new ReportDAL().GetCustomerBill(customer.ID, int.Parse(month), int.Parse(year));
-            var customerList = new ReportDAL().GetCustomerBill(customer.ID, int.Parse(month), int.Parse(year));
+            var customerBill = new ReportDAL().GetCustomerBill(customer.ID, int.Parse(month), int.Parse(year));
 
-            reportDataSource.Value = customerList;
+            reportDataSource.Value = customerBill;
             localReport.DataSources.Add(reportDataSource);
 
             string period = string.Format("{0}/{1}", month, year);
@@ -78,10 +77,19 @@ namespace TMBF.Controllers
             byte[] renderedBytes;
             //Render the report            
             renderedBytes = localReport.Render(reportType, deviceInfo, out mimeType, out encoding, out fileNameExtension, out streams, out warnings);
-            //Response.AddHeader("content-disposition", "attachment; filename=NorthWindCustomers." + fileNameExtension); 
-  
-                return File(renderedBytes, "image/jpeg");
 
+            if (format == null)
+            {
+                return File(renderedBytes, "image/jpeg");
+            }
+            else if (format == "pdf")
+            {
+                return File(renderedBytes, "pdf");
+            }
+            else
+            {
+                return File(renderedBytes, "image/jpeg");
+            }
         }    
 
     }
