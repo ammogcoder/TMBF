f7a01ec494b4be2ad8c2ff218e1a2b5b5db80cd9
TuanDang <dangtuan21@gmail.com>
2014-12-03 21:07:49 -0600

    Add CallExists

--- TMBF/TMBF/Business/CallsDataAccess.cs
+++ TMBF/TMBF/Business/CallsDataAccess.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using TMBF.DAL;
+using TMBF.Models;
 
 namespace TMBF.Business
 {
     public class CallsDataAccess
     {
+        //Check if CallExist, by tuandang
+        private static Boolean CallExists(DateTime CallDate, double CallTime, double Duration, string ReceiverNo, double CustomerID, double SourceCountry_ID, double DestinationCountry_ID)
+        {
+            TelecomContext db = new TelecomContext();
+            Call call = db.Calls.Where(c => c.CustomerID == CustomerID && c.ReceiverNo == ReceiverNo &&
+                c.CallDate == CallDate && c.CallTime == CallTime).FirstOrDefault();
+            return call != null;
+        }
         public static Boolean insertCalls(DateTime CallDate, double CallTime, double Duration, string ReceiverNo, double CustomerID, double SourceCountry_ID, double DestinationCountry_ID)
         {
+            //Check if CallExist, by tuandang
+            if (CallExists(CallDate, CallTime, Duration, ReceiverNo, CustomerID, SourceCountry_ID,  DestinationCountry_ID))
+                return true;
             int ret = 0;
             string sql = @"INSERT [Call] ([CallDate], [CallTime], [Duration], [ReceiverNo], [CustomerID], [DestinationCountry_ID], [SourceCountry_ID]) 
 VALUES (CAST(@CallDate AS DateTime), @CallTime, @Duration, @ReceiverNo, @CustomerID,@DestinationCountry_ID, @SourceCountry_ID)";
